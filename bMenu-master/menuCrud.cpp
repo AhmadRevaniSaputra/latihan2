c3d2665569cd5a749a10b0602727f0c77edc9efd
AhmadRevaniSaputra <ahmadrevanis27@gmail.com>
2024-12-27 19:16:41 +0800

    Menambahkan fungsi Read

--- bMenu-master/menuCrud.cpp
+++ bMenu-master/menuCrud.cpp
@@ -4,12 +4,15 @@
 using namespace std;
 
 struct mahasiswa {
-@@ -12,50 +13,84 @@ struct mahasiswa {
+    string nim;
+    string nama;
+    string alamat;
+    float ipk;
+};
+
 mahasiswa sikc[30];
 int pos = -1;
 
-void tampilkanData(int p) {
-    cout << sikc[p].nim << " , " << sikc[p].nama << " , " << sikc[p].alamat << " , " << sikc[p].ipk << endl;
 // Fungsi untuk membersihkan layar (cross-platform)
 void clearScreen() {
 #ifdef _WIN32
@@ -26,42 +29,26 @@ void masukanData() {
         return;
     }
     pos++;
-    system("cls");
-    fflush(stdin);
-    cout << "masukan nim: ";
     clearScreen();
     cout << "Masukan NIM: ";
     getline(cin, sikc[pos].nim);
-    cout << "masukan nama: ";
     cout << "Masukan Nama: ";
     getline(cin, sikc[pos].nama);
-    cout << "masukan alamat: ";
     cout << "Masukan Alamat: ";
     getline(cin, sikc[pos].alamat);
-    cout << "masukan ipk: ";
     cout << "Masukan IPK: ";
     cin >> sikc[pos].ipk;
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
     cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Membersihkan input buffer
     cout << "Data berhasil ditambahkan!\n";
 }
 
-void perbaikanData(int p) {
-    system("cls");
-    fflush(stdin);
-    cout << "masukan nama: ";
-    getline(cin, sikc[p].nama);
-    cout << "masukan alamat: ";
-    getline(cin, sikc[p].alamat);
-    cout << "masukan ipk: ";
-    cin >> sikc[p].ipk;
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
 // Fungsi untuk memasukkan banyak data mahasiswa
 void masukanBanyakData() {
     int jumlah;
     cout << "Masukan jumlah data yang ingin ditambahkan: ";
     cin >> jumlah;
     cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Membersihkan input buffer
+
     for (int i = 0; i < jumlah; i++) {
         if (pos >= 29) { // Membatasi jumlah maksimal data
             cout << "Data penuh! Tidak bisa menambahkan lebih banyak data.\n";
@@ -72,19 +59,25 @@ void masukanBanyakData() {
     }
 }
 
+// Fungsi untuk menampilkan semua data mahasiswa
+void tampilkanSemuaData() {
+    clearScreen();
+    if (pos == -1) { // Jika tidak ada data
+        cout << "Belum ada data yang dimasukkan.\n";
+        return;
+    }
+    cout << "Data Mahasiswa:\n";
+    cout << "------------------------------------\n";
+    for (int i = 0; i <= pos; i++) {
+        cout << "Data ke-" << i + 1 << ":\n";
+        cout << "NIM     : " << sikc[i].nim << endl;
+        cout << "Nama    : " << sikc[i].nama << endl;
+        cout << "Alamat  : " << sikc[i].alamat << endl;
+        cout << "IPK     : " << sikc[i].ipk << endl;
+        cout << "------------------------------------\n";
+    }
+}
 int main() {
-    // ujicoba memasukan data di array index pertama
-    masukanData();
-    // ujicoba memasukan data di array index kedua
-    masukanData();
-    // ujicoba tampilkan data terakhir input
-    tampilkanData(pos);
-    // ujicoba tampilkan data pada array pertama
-    tampilkanData(0);
-    // ujicoba merubah data pada array pertama
-    perbaikanData(0);
-    // ujicoba merubah data pada array kedua
-    tampilkanData(0);
     int pilihan;
     do {
         clearScreen();
@@ -92,9 +85,12 @@ int main() {
         cout << "1. Masukkan Data\n";
         cout << "2. Masukkan Banyak Data\n";
         cout << "3. Keluar\n";
+        cout << "3. Tampilkan Semua Data\n";
+        cout << "4. Keluar\n";
         cout << "Pilihan: ";
         cin >> pilihan;
         cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Membersihkan input buffer
+
         switch (pilihan) {
         case 1:
             masukanData();
@@ -103,16 +99,23 @@ int main() {
             masukanBanyakData();
             break;
         case 3:
+            tampilkanSemuaData();
+            break;
+        case 4:
             cout << "Keluar dari program.\n";
             break;
         default:
             cout << "Pilihan tidak valid.\n";
             break;
         }
+
         if (pilihan != 3) {
+        if (pilihan != 4) {
             cout << "\nTekan Enter untuk kembali ke menu...";
             cin.get();
         }
     } while (pilihan != 3);
+    } while (pilihan != 4);
+
     return 0;
 }
